61f0421de07eef525f7e52f4988175ab9f057d93
Sadin97 <sadinpita_97@windowslive.com>
2018-09-26 15:32:33 +0200

    changes

--- routes/users.js
+++ routes/users.js
@@ -26,13 +26,19 @@ router.get('/user/:id', function (req, res) {
 router.put('/user/:id', function (req, res) {
     let user_id = req.params.id;
     let name = req.body.name;
-    if (!user_id) {
-        return res.status(400).send({ error: true, message: 'Please provide user id.' });
-    }
-    db.query('UPDATE users SET name = ? WHERE id = ?', [name, user_id], function (error, results, fields) {
-        if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'Successfully updated.' });
-    });
+
+
+
+    res.send(JSON.stringify(req.body));
+
+
+    // if (!user_id) {
+    //     return res.status(400).send({ error: true, message: 'Please provide user id.' });
+    // }
+    // db.query('UPDATE users SET name = ? WHERE id = ?', [name, user_id], function (error, results, fields) {
+    //     if (error) throw error;
+    //     return res.send({ error: false, data: results[0], message: 'Successfully updated.' });
+    // });
 });
 
 module.exports = router;
