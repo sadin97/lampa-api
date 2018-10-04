2b7cc908469d17590a223b58a30f3e255432ce1b
Sadin97 <sadinpita_97@windowslive.com>
2018-10-04 15:41:01 +0200

    implemented crypto, implemented new registration process for user

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database');
-
-const bcrypt = require('bcryptjs');
-
+var uuid = require('uuid');
+var bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 router.post('/login', async function (req, res) {
@@ -13,7 +12,9 @@ router.post('/login', async function (req, res) {
   if (!email || !textPassword) {
     return res.status(400).send({ error: true, message: 'Please provide: email (string) and textPassword (string).' });
   }
-
+  // console.log(uuid.v1());
+  // console.log(uuid.v3());
+  // console.log(uuid.v4());
   db.query('SELECT salt, password, name FROM users where email = ?', email, function (error, results, fields) {
       if (error) throw error;
       let salt = results[0]['salt'];
@@ -23,7 +24,18 @@ router.post('/login', async function (req, res) {
 
       let loginInput = bcrypt.hashSync(textPassword, salt);
 
-      bcrypt.compare(loginInput, passHashed, function(err, res) {
+      console.log('passHashed iz baze: ', passHashed);
+      console.log('password iz requesta hashan i saltiran ', loginInput)
+
+      // if (loginInput === passHashed) {
+      //   console.log('Password matches!');
+      // } else {
+      //   console.log('Password don\'t match!');
+      // }
+
+      bcrypt.compare(textPassword, passHashed, function(err, res) {
+        console.log("err", err);
+        console.log("res", res);
         if (res) {
          console.log('Password matches!');
         } else {
--- routes/registration.js
+++ routes/registration.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database');
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 
+var genRandomString = function(length) {
+  return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex') /** convert to hexadecimal format */
+    .slice(0,length);   /** return required number of characters */
+};
+
+var sha512 = function(password, salt){
+  var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  var value = hash.digest('hex');
+  return {
+      salt:salt,
+      passwordHash:value
+  };
+};
+
+function saltHashPassword(userpassword) {
+    var salt = genRandomString(16); /** Gives us salt of length 16 */
+    var passwordData = sha512(userpassword, salt);
+
+}
 
 // Add a new user.
 router.post('/registration', function (req, res) {
@@ -14,8 +36,14 @@ router.post('/registration', function (req, res) {
   let email = req.body.email;
   let textPassword = req.body.password;
 
-  let salt = bcrypt.genSaltSync(saltRounds);
-  let hash = bcrypt.hashSync(textPassword, salt);
+  var salt = genRandomString(16); /** Gives us salt of length 16 */
+  var passwordData = sha512(textPassword, salt);
+
+  console.log('UserPassword = ' + textPassword);
+  console.log('Passwordhash = ' + passwordData.passwordHash);
+  console.log('nSalt = ' + passwordData.salt);
+  // let salt = bcrypt.genSaltSync(saltRounds);
+  // let hash = bcrypt.hashSync(textPassword, salt);
 
   // Technique 1 (generate a salt and hash on separate function calls).
   // bcrypt.genSalt(saltRounds, function(err, salt) {
@@ -30,9 +58,9 @@ router.post('/registration', function (req, res) {
       return res.status(400).send({ error: true, message: 'Please provide: name (string), email (string) and textPassword (string).' });
   }
 
-  db.query("INSERT INTO users SET name = ?, email = ?, password = ?, salt = ?", [name, email, hash, salt], function (error, results, fields) {
+  db.query("INSERT INTO users SET name = ?, email = ?, password = ?, salt = ?", [name, email, passwordData.passwordHash, passwordData.salt], function (error, results, fields) {
       if (error) throw error;
-      return res.send({  error: false, data: results, message: `'${textPassword}' is my text password.\n'${hash}' is hash.\n'${salt}' is salt.` })
+      return res.send({  error: false, data: results, message: `'${textPassword}' is my text password.\n'${passwordData.passwordHash}' is hash.\n'${passwordData.salt}' is salt.` })
       });
 });
 
